b837ccc18a4e4ccb716978e3c1c4447d3ac7584e
Anton M Hengst <antonmhengst@gmail.com>
2024-07-29 23:15:06 -0700

    Satisifactory mesh for first attempt

---
+++ chamber.geo
@@ -0,0 +1,148 @@
+// Gmsh project created on Mon Jul 29 21:07:00 2024
+c0 = 0.1;
+c1 = 0.2;
+c2 = 0.4;
+n = 5;
+
+Rc = 0.7;
+iwt = 0.05;
+ct = 0.1;
+owt = 0.25;
+Lcyl = 3.93;
+R2 = 0.46;
+b = 45*Pi/180;
+Rt = 0.3;
+Lc = 4.71;
+R1 = 0.45;
+Rn = 0.11;
+Te = 15*Pi/180;
+Le = 2.17;
+Re = 0.875;
+//+
+//Point(1) =    {0, 0, 0, c};
+//Point(1000) = {0, Lc+Le, 0, c};
+//Line(1) = {1, 1000};
+//+
+x1 = Rc;
+x2 = Rc+iwt;
+x3 = Rc+iwt+ct;
+x4 = Rc+iwt+ct+owt;
+Point(2) = {x1, 0, 0, c1};
+Point(3) = {x2, 0, 0, c0};
+Point(4) = {x3, 0, 0, c0};
+Point(5) = {x4, 0, 0, c2};
+Line(2) = {2, 3};
+Line(3) = {3, 4};
+Line(4) = {4, 5};
+//+
+Point(7) = {x1,   Lcyl, 0, c1};
+Point(8) = {x2,   Lcyl, 0, c0};
+Point(9) = {x3,   Lcyl, 0, c0};
+Point(10) = {x4,  Lcyl, 0, c2};
+Line(6) = {2, 7};
+Line(7) = {3, 8};
+Line(8) = {4, 9};
+Line(9) = {5, 10};
+//+
+Point(11) = {x1-R2, Lcyl, 0, c1};
+Point(12) = {x2-R2, Lcyl, 0, c0};
+Point(13) = {x3-R2, Lcyl, 0, c0};
+Point(14) = {x4-R2, Lcyl, 0, c2};
+Point(15) = {x1-R2*(1-Cos(b)), Lcyl+R2*Sin(b), 0, c1};
+Point(16) = {x2-R2*(1-Cos(b)), Lcyl+R2*Sin(b), 0, c0};
+Point(17) = {x3-R2*(1-Cos(b)), Lcyl+R2*Sin(b), 0, c0};
+Point(18) = {x4-R2*(1-Cos(b)), Lcyl+R2*Sin(b), 0, c2};
+Circle(10) = {7, 11, 15};
+Circle(11) = {8, 12, 16};
+Circle(12) = {9, 13, 17};
+Circle(13) = {10, 14, 18};
+//+
+x5 = Rt;
+x6 = Rt+iwt;
+x7 = Rt+iwt+ct;
+x8 = Rt+iwt+ct+owt;
+Point(19) = {x5, Lc, 0, c1};
+Point(20) = {x6, Lc, 0, c0};
+Point(21) = {x7, Lc, 0, c0};
+Point(22) = {x8, Lc, 0, c2};
+//+
+Point(23) = {x5+R1, Lc, 0, c1};
+Point(24) = {x6+R1, Lc, 0, c0};
+Point(25) = {x7+R1, Lc, 0, c0};
+Point(26) = {x8+R1, Lc, 0, c2};
+Point(27) = {x5+R1*(1-Cos(b)), Lc-R1*Sin(b), 0, c1};
+Point(28) = {x6+R1*(1-Cos(b)), Lc-R1*Sin(b), 0, c0};
+Point(29) = {x7+R1*(1-Cos(b)), Lc-R1*Sin(b), 0, c0};
+Point(30) = {x8+R1*(1-Cos(b)), Lc-R1*Sin(b), 0, c2};
+Circle(20) = {19, 23, 27};
+Circle(21) = {20, 24, 28};
+Circle(22) = {21, 25, 29};
+Circle(23) = {22, 26, 30};
+Line(24) = {15, 27};
+Line(25) = {16, 28};
+Line(26) = {17, 29};
+Line(27) = {18, 30};
+//+
+Point(31) = {x5+Rn, Lc, 0, c1};
+Point(32) = {x6+Rn, Lc, 0, c0};
+Point(33) = {x7+Rn, Lc, 0, c0};
+Point(34) = {x8+Rn, Lc, 0, c2};
+Point(35) = {x5+Rn*(1-Cos(Te)), Lc+Rn*Sin(Te), 0, c1};
+Point(36) = {x6+Rn*(1-Cos(Te)), Lc+Rn*Sin(Te), 0, c0};
+Point(37) = {x7+Rn*(1-Cos(Te)), Lc+Rn*Sin(Te), 0, c0};
+Point(38) = {x8+Rn*(1-Cos(Te)), Lc+Rn*Sin(Te), 0, c2};
+Circle(28) = {19, 31, 35};
+Circle(29) = {20, 32, 36};
+Circle(30) = {21, 33, 37};
+Circle(31) = {22, 24, 38};
+//+
+x10 = Re;
+x11 = Re+iwt;
+x12 = Re+iwt+ct;
+x13 = Re+iwt+ct+owt;
+Point(40) = {x10, Lc+Le, 0, c1};
+Point(41) = {x11, Lc+Le, 0, c0};
+Point(42) = {x12, Lc+Le, 0, c0};
+Point(43) = {x13, Lc+Le, 0, c2};
+Line(32) = {35, 40};
+Line(33) = {36, 41};
+Line(34) = {37, 42};
+Line(35) = {38, 43};
+//+
+Line(36) = {40, 41};
+Line(37) = {41, 42};
+Line(38) = {42, 43};
+//+
+Curve Loop(1) = {32, 36, -33, -29, 21, -25, -11, -7, -2, 6, 10, 24, -20, 28};
+Plane Surface(1) = {1};
+//+
+Curve Loop(2) = {7, 11, 25, -21, 29, 33, 37, -34, -30, 22, -26, -12, -8, -3};
+Plane Surface(2) = {2};
+//+
+Curve Loop(3) = {35, -38, -34, -30, 22, -26, -12, -8, 4, 9, 13, 27, -23, 31};
+Plane Surface(3) = {3};
+//+
+Extrude {{0, 1, 0}, {0, 0, 0}, 2*Pi/33} { Surface{1}; }
+Extrude {{0, 1, 0}, {0, 0, 0}, 2*Pi/33} { Surface{3}; }
+Extrude {{0, 1, 0}, {0, 0, 0}, 2*Pi/33/4} { Surface{2}; }
+Extrude {{0, 1, 0}, {0, 0, 0}, 2*Pi/33/2} { Surface{254}; }
+Extrude {{0, 1, 0}, {0, 0, 0}, 2*Pi/33/4} { Surface{326}; }
+//+
+Physical Volume("Channel", 399) = {4};
+Physical Volume("Metal", 400) = {1, 3, 5, 2};
+//+
+Physical Surface("Outlet", 401) = {325};
+Physical Surface("Inlet", 402) = {297};
+//+
+Physical Surface("Top", 403) = {161, 397, 253, 89};
+Physical Surface("Bottom", 404) = {133, 225, 61, 369};
+//+
+Physical Surface("Interior", 405) = {57, 109, 105, 101, 97, 93};
+Physical Surface("Exterior", 406) = {129, 181, 177, 173, 169, 165};
+//+
+Physical Surface("Walls", 407) = {273, 277, 281, 285, 289, 293, 254, 326, 301, 305, 309, 313, 317, 321};
+//+
+Physical Surface("Symmetry", 408) = {182, 3, 2, 1, 398, 110};
+//+
+channel_points[] = PointsOf{ Surface{325, 297, 273, 277, 281, 285, 289, 293, 254, 326, 301, 305, 309, 313, 317, 321}; };
+Characteristic Length{ channel_points[] } = 0.05;
--- main.cpp
+++ main.cpp
@@ -1,41 +1,116 @@
+// This code simulates the Stokes flow of a viscous fluid (water) in a microvalve.
+// Reynold's number rho*v*L/mu is very low so that the inertia terms can be neglected.
+// The fluid is considered incompressible.
+//
+// This code can be validated with analytical solutions of a flow between two
+// infinite parallel plates with a thin gap. Only the geometry has to be adapted.
+
+
 #include "sparselizard.h"
+
 
 using namespace sl;
 
+mesh createmesh(void);
+
 int main(void)
-{	
-    // The domain regions as defined in 'disk.geo':
-    int vol = 1, sur = 2, top = 3;
+{
+    // Define the physical regions that will be used:
+    int channel = 399, metal = 400, outlet = 401, inlet = 402, top = 403, bottom = 404;
+    int interior = 405, exterior = 406, walls = 407, symmetry = 408;
     
-    // The mesh can be curved!
-    mesh mymesh("disk.msh");
+    // Create the geometry and the mesh:    
+    mesh mymesh = createmesh();
     
-    // Nodal shape functions 'h1' with 3 components.
-    // Field u is the membrane deflection.
-    field u("h1xyz");
+    // Write the mesh for display:
+    mymesh.write("channel.msh");
 
-    // Use interpolation order 2 on 'vol', the whole domain:
-    u.setorder(vol, 2);
-    
-    // Clamp on surface 'sur' (i.e. 0 valued-Dirichlet conditions):
-    u.setconstraint(sur);
-  
-    // E is Young's modulus. nu is Poisson's ratio. 
-    parameter E, nu;
-    E|vol = 150e9; nu|vol = 0.3;
-  
-    formulation elasticity;
 
-    // The linear elasticity formulation is classical and thus predefined:
-    elasticity += integral(vol, predefinedelasticity(dof(u), tf(u), E, nu));
-    // Add a volumic force in the -z direction:
-    elasticity += integral(vol, array3x1(0,0,-10)*tf(u));
-
-    // Generate, solve and transfer the solution to field u:
-    elasticity.solve();
+    // Define the fluid boundary as the intersection between the solid and the fluid regions:
+    int fluidboundary = selectintersection({channel,metal});
     
-    // Write the deflection to ParaView .vtk format.
-    // Write with an order 2 interpolation. Exaggerate the deflection by a factor 0.5e9.
-    (0.5e9*u).write(vol, "output.vtk", 2);
+    // Dynamic viscosity of water [Pa.s] and density [kg/m3]:
+    double mu = 8.9e-4, rho = 1000;
+    
+    // Field v is the flow velocity. It uses nodal shape functions "t2" with two components.
+    // Field p is the relative pressure.
+    field v("t2xyz"), p("t2");
+    
+    // Force the flow velocity to 0 at the solid interface:
+    v.setconstraint(fluidboundary);
+    
+    // Set a relative pressure of 100 Pa at the valve inlet and 0 at the outlet:
+    p.setconstraint(inlet, 100);
+    p.setconstraint(outlet);
+    
+    // Use an order 1 interpolation for p and 2 for v on the fluid region:
+    p.setorder(fluid, 1);
+    v.setorder(fluid, 2);
+    
+    // Define the weak formulation of the Stokes flow problem.
+    // The strong form can be found at https://en.wikipedia.org/wiki/Stokes_flow
+    formulation viscousflow;
+
+    viscousflow += integral(fluid, predefinedstokes(dof(v), tf(v), dof(p), tf(p), mu, rho, 0, 0) );	
+    
+    // Generate, solve and save:
+    viscousflow.solve();
+    
+    // Write the p and v fields to file:
+    p.write(fluid, "p.vtk", 1);
+    v.write(fluid, "v.vtk", 2);
+    
+    // Output the flowrate for a unit width:
+    double flowrate = (-normal(fluid)*v).integrate(inlet, 4);
+    std::cout << std::endl << "Flowrate for a unit width: " << flowrate << " m^3/s" << std::endl;
+    
+    // Code validation line. Can be removed.
+    std::cout << (flowrate < 1.4845e-7 && flowrate > 1.4844e-7);
+}
+
+mesh createmesh(void)
+{
+    // Give names to the physical region numbers:
+    int support = 1, fluid = 2, pillar = 3, membrane = 4, inlet = 7, outlet = 8;
+
+    // Define the x and y geometrical dimensions:
+    double y1 = 15e-6, y2 = 25e-6, y3 = 30e-6;
+    double x1 = -100e-6, x2 = -50e-6, x3 = -25e-6, x4 = 25e-6, x5 = 50e-6, x6 = 100e-6;
+    
+    // Define the mesh finesse:
+    int n = 10;
+    
+    shape q1("quadrangle", support, {x1,0,0, x2,0,0, x2,y1,0, x1,y1,0}, {n,n,n,n});
+    shape q2("quadrangle", support, {x5,0,0, x6,0,0, x6,y1,0, x5,y1,0}, {n,n,n,n});
+    
+    shape q3("quadrangle", fluid, {x2,0,0, x3,0,0, x3,y1,0, x2,y1,0}, {n,n,n,n});
+    shape q4("quadrangle", fluid, {x4,0,0, x5,0,0, x5,y1,0, x4,y1,0}, {n,n,n,n});
+    
+    shape q5("quadrangle", pillar, {x3,0,0, x4,0,0, x4,y1,0, x3,y1,0}, {n,n,n,n});
+    
+    shape q6("quadrangle", support, {x1,y1,0, x2,y1,0, x2,y2,0, x1,y2,0}, {n,n,n,n});
+    shape q7("quadrangle", support, {x5,y1,0, x6,y1,0, x6,y2,0, x5,y2,0}, {n,n,n,n});
+    
+    shape q8("quadrangle", fluid, {x2,y1,0, x3,y1,0, x3,y2,0, x2,y2,0}, {n,n,n,n});
+    shape q9("quadrangle", fluid, {x4,y1,0, x5,y1,0, x5,y2,0, x4,y2,0}, {n,n,n,n});
+    shape q10("quadrangle", fluid, {x3,y1,0, x4,y1,0, x4,y2,0, x3,y2,0}, {n,n,n,n});
+    
+    shape q11("quadrangle", membrane, {x1,y2,0, x2,y2,0, x2,y3,0, x1,y3,0}, {n,n,n,n});
+    shape q12("quadrangle", membrane, {x5,y2,0, x6,y2,0, x6,y3,0, x5,y3,0}, {n,n,n,n});
+    shape q13("quadrangle", membrane, {x2,y2,0, x3,y2,0, x3,y3,0, x2,y3,0}, {n,n,n,n});
+    shape q14("quadrangle", membrane, {x4,y2,0, x5,y2,0, x5,y3,0, x4,y3,0}, {n,n,n,n});
+    shape q15("quadrangle", membrane, {x3,y2,0, x4,y2,0, x4,y3,0, x3,y3,0}, {n,n,n,n});
+    
+    // Get the inlet and outlet lines:
+    shape l1 = q3.getsons()[0];
+    shape l2 = q4.getsons()[0];
+    
+    l1.setphysicalregion(inlet);
+    l2.setphysicalregion(outlet);
+    
+    // Provide to the mesh all shapes of interest:
+    mesh mymesh({q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,l1,l2});
+    
+    return mymesh;
 }
 
